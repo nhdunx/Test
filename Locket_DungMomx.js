aecbac098d742eaa262ad17c4038ea45b86df7b5
nhdunx <dungvipboy0@gmail.com>
2025-05-06 21:30:37 +0700

    Update Locket_DungMomx.js

--- Locket_DungMomx.js
+++ Locket_DungMomx.js
@@ -1,6 +1,43 @@
 const mapping = {
-  '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
   'Locket': ['Gold']
 };
 
-var ua=$request.headers["User-Agent"]||$request.headers["user-agent"],obj=JSON.parse($response.body);obj.Attention="UP TO NỖI HÀ DỤNG";var dunx={is_sandbox:!1,ownership_type:"PURCHASED",billing_issues_detected_at:null,period_type:"normal",expires_date:"2222-12-21T01:23:45Z",grace_period_expires_date:null,unsubscribe_detected_at:null,original_purchase_date:"2004-12-21T01:23:45Z",purchase_date:"2004-12-21T01:23:45Z",store:"app_store"},titkok={grace_period_expires_date:null,purchase_date:"2004-12-21T01:23:45Z",product_identifier:"com.dunx.premium.yearly",expires_date:"2222-12-21T01:23:45Z"};const match=Object.keys(mapping).find(e=>ua.includes(e));if(match){let[e,s]=mapping[match];s?(titkok.product_identifier=s,obj.subscriber.subscriptions[s]=dunx):obj.subscriber.subscriptions["com.dunx.premium.yearly"]=dunx,obj.subscriber.entitlements[e]=titkok}else obj.subscriber.subscriptions["com.dunx.premium.yearly"]=dunx,obj.subscriber.entitlements.pro=titkok;$done({body:JSON.stringify(obj)});
+var ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
+var obj = JSON.parse($response.body);
+
+// Base entitlement info
+let entitlementInfo = {
+  grace_period_expires_date: null,
+  purchase_date: "2024-01-01T00:00:00Z",
+  product_identifier: "com.locket.gold.yearly",
+  expires_date: "2099-12-31T23:59:59Z",
+  is_sandbox: false,
+  unsubscribe_detected_at: null,
+  billing_issues_detected_at: null,
+  is_active: true
+};
+
+// Gán vào entitlements
+obj.subscriber.entitlements = {
+  Gold: entitlementInfo
+};
+
+// Gán vào subscriptions
+obj.subscriber.subscriptions = {
+  "com.locket.gold.yearly": {
+    ...entitlementInfo,
+    original_purchase_date: "2024-01-01T00:00:00Z",
+    ownership_type: "PURCHASED",
+    store: "app_store"
+  }
+};
+
+// Các thuộc tính bổ sung để hiển thị huy hiệu
+obj.subscriber.profileBadge = "locket_gold_badge";
+obj.subscriber.hasActiveBadges = true;
+obj.subscriber.badgeCount = 1;
+obj.subscriber.profileBadgeEnabled = true;
+obj.subscriber.is_active = true;
+obj.subscriber.goldMaxDuration = 120; // Giới hạn độ dài video 120 giây nếu có dùng
+
+$done({ body: JSON.stringify(obj) });
